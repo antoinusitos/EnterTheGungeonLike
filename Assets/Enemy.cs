fa8f48ade0b71024a387c317a51f9d7717bb0193
TDubrulle <Thomasdubrulle25@hotmail.fr>
2017-01-16 11:22:25 +0100

    Added enemy prefab + script

---
+++ Assets/Enemy.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[System.Serializable]
+public class EnemyParameters
+{
+    public float minRangeToAttack = 10f;
+    public float stopDistance = 5f;
+    public float minRangeToPursue = 20.0f;
+}
+
+public class Enemy : MonoBehaviour {
+
+    private NavMeshAgent linkedAgent;
+
+    public EnemyParameters enemyParam;
+
+    public Transform target;
+    private Transform tr;
+
+    // Use this for initialization
+    void Awake () {
+        if (target == null)
+        {
+            target = findTarget();
+        }
+        linkedAgent = this.GetComponent<NavMeshAgent>();
+        Debug.Assert(target != null);
+        tr = this.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveToTarget();
+        attackTarget();
+	}
+
+    Transform findTarget()
+    {
+        return GameObject.FindGameObjectsWithTag("player")[0].transform;
+    }
+    
+
+    public void moveToTarget()
+    {
+        float dist = Vector3.Distance(this.tr.position, target.position);
+        if (dist < enemyParam.minRangeToPursue)
+        {
+            linkedAgent.SetDestination(target.position);
+        }
+        if(dist < enemyParam.stopDistance)
+        {
+            linkedAgent.Stop();
+        }
+    }
+
+    void attackTarget()
+    {
+        if (Vector3.Distance(this.tr.position, target.position) < enemyParam.minRangeToAttack) {
+            //Attack
+        }
+    }
+
+}
